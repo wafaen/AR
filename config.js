9b0a40948d3bc726ef7eae1ebf499913aeed8867
Wafae <wafaechanart@gmail.com>
2026-06-26 21:32:03 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+// Edit these two numbers to match your printed target image size/aspect.
+// Example A3 portrait is 297 x 420. Square is 1 x 1.
+window.AR_CONFIG = {
+  targetWidth: 1,
+  targetHeight: 1,
+  pauseWhenTargetLost: false,
+  restartFromBeginningOnFound: false
+};
