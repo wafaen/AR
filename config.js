52b43b737a3c93c344de0bffa3e587f04dfb2a6f
Wafae <wafaechanart@gmail.com>
2026-06-26 21:52:45 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Edit these two numbers to match your printed target image size/aspect.
-// Example A3 portrait is 297 x 420. Square is 1 x 1.
-window.AR_CONFIG = {
-  targetWidth: 1,
-  targetHeight: 1,
-  pauseWhenTargetLost: false,
-  restartFromBeginningOnFound: false
-};
